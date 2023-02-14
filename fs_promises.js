c4368684bfeabd6902a89e856b696260e57c496c
alexandrminskiy <alexandrminskiy@gmail.com>
2023-02-14 21:06:28 +0300

    2

--- fs_promises.js
+++ fs_promises.js
@@ -2,10 +2,10 @@
 const fs = require('fs')
 
 const readFilePromise = (pathToFile, encoding = 'utf-8') => {
-    return new Promise((resolve, reject)=>{
+    return new Promise((resolve, reject) => {
         fs.readFile(pathToFile, encoding, (errRead, contentOfFile) => {
             if (errRead) {
-            reject(errRead)
+                reject(errRead)
             }
             resolve(contentOfFile)
         })
@@ -13,20 +13,27 @@ const readFilePromise = (pathToFile, encoding = 'utf-8') => {
 }
 
 const writeFilePromise = (pathToFile, content) => {
-return new Promise((resolve, reject)=>{
-    fs.writeFile('pathToFile, content, (errWrite) => {
-        if (errWrite) {
-            reject(errWrite)
-        }
-        resolve('Файл был создан по пути ${pathToFile}')
+    return new Promise((resolve, reject) => {
+        fs.writeFile(pathToFile, content, (errWrite) => {
+            if (errWrite) {
+                reject(errWrite)
+            }
+            resolve(`Файл был создан по пути ${pathToFile}`)
+        })
     })
-})
 }
 const orignFilePath = './index.js'
 const newFilePath = './index_copy.js'
 
 readFilePromise(orignFilePath)
-.then((data)=>{
-console.log({data})
-})
-.catch( console.log )
+    .then((data) => {
+        const newContent = data + data
+        return writeFilePromise(newFilePath, newContent)
+    })
+    .then((dataFromPrevThen)=>console.log({dataFromPrevThen}))
+    // .catch((e)=>{
+    //     console.log(e)
+    // })
+    .catch(console.log())
+
+    2
